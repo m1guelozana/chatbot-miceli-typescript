5543f4cacfc6ed1bb70acc9a81fc24c6d360515a
Miguel Ozana <miguelozana74@gmail.com>
2024-05-16 12:54:31 -0300

    work

--- src/whatsapp/client.ts
+++ src/whatsapp/client.ts
@@ -11,6 +11,11 @@ export async function initializeWhatsAppClient() {
         headless: true,
         args: ["--no-sandbox", "--disable-setuid-sandbox"],
       },
+      webVersionCache: {
+        type: "remote",
+        remotePath:
+          "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+      },
     });
 
     client.on("qr", (qr) => {
