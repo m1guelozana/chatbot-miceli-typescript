20e40a778981a782583b401f6c5b97ca9e637796
Miguel Ozana <miguelozana74@gmail.com>
2024-05-28 10:05:13 -0300

    new commits

--- src/whatsapp/messages/first-message.ts
+++ src/whatsapp/messages/first-message.ts
@@ -4,22 +4,15 @@ import handleOption1 from "./options/message-option-one";
 import handleOption2 from "./options/message-option-two";
 import handleOption3 from "./options/message-option-three";
 import handleOption4 from "./options/message-option-four";
-import stateManager from "../../state";
 
 const handleUserFirstMessage = async (client: Client, message: Message) => {
     const chatId = message.from;
     const chat = await message.getChat();
     
-    // Verifica se o usuário já está em uma conversa
-    if (stateManager.isUserInConversation(chatId)) {
-        console.log("User is already in a conversation. Skipping welcome message.");
-        return;
-    }
 
     console.log("Handling user first message");
 
     try {
-        stateManager.setUserState(chatId, 'initial');
         await client.sendMessage(
             message.from,
             "Olá!\nObrigado por entrar em contato conosco. Escolha uma opção para continuarmos.\n[1]*Conversar com um Especialista*\n[2]*Conversar com setor Financeiro*\n[3]*Conversar com setor de RH*\n[4]*Conversar com setor Comercial*"
@@ -31,16 +24,16 @@ const handleUserFirstMessage = async (client: Client, message: Message) => {
         // Encaminhar para o tratamento da opção selecionada
         switch (userChoice) {
             case "1":
-                stateManager.setUserState(chatId, 'option1');
+                await handleOption1(client, message)
                 break;
             case "2":
-                stateManager.setUserState(chatId, 'option2');
+                await handleOption2(client, message)
                 break;
             case "3":
-                stateManager.setUserState(chatId, 'option3');
+                await handleOption3(client, message)
                 break;
             case "4":
-                stateManager.setUserState(chatId, 'option4');
+                await handleOption4(client, message)
                 break;
             default:
                 await client.sendMessage(message.from, 'Opção Inválida. Por favor, selecione uma opção válida.');
