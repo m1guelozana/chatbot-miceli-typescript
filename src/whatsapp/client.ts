c8a0ddb2966a1d99ae0561817d72f89830516f0f
Miguel Ozana <miguelozana74@gmail.com>
2024-06-06 12:18:17 -0300

    commit

--- src/whatsapp/client.ts
+++ src/whatsapp/client.ts
@@ -19,7 +19,7 @@ export async function initializeWhatsAppClient(): Promise<void> {
     client = new Client({
         authStrategy: new LocalAuth(),
         puppeteer: {
-            headless: false,
+            headless: true,
             args: ["--no-sandbox", "--disable-setuid-sandbox"],
             timeout: 0,
         },
